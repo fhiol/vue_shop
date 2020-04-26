c1d3a5d28296119b3fa05e61abd825b3f77a3654
fhiol <1722885779@qq.com>
2020-04-26 16:02:15 +0800

    完成了用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -7,8 +7,18 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from "axios";
+import { MessageBox } from 'element-ui'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+
 Vue.prototype.$http = axios
+Vue.prototype.$confirmhttp = MessageBox.confirm
 
 Vue.config.productionTip = false
 
